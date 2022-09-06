11096bd82996c26ef796186dd72b547a6e43548a
Lisia Grocott <lisiagrocott@gmail.com>
2022-09-07 11:03:01 +1200

    fix deps and versions so React component renders :)

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 import App from "../components/index";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   render(<App />, document.body.appendChild(document.createElement("div")));
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  render(<App />, document.body.appendChild(document.createElement("div")));
+});
 
-const root = createRoot(document.documentElement);
-const element = <App />;
-root.render(element);
+// const root = createRoot(document.documentElement);
+// const element = <App />;
+// root.render(element);

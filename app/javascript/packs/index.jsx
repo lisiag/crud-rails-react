f63fa34c485f3566912de3bffacecdb69fbe3eab
Lisia Grocott <lisiagrocott@gmail.com>
2022-09-06 20:44:40 +1200

    fix import filename

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "../components/App";
+import App from "../components/index";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(<App />, document.body.appendChild(document.createElement("div")));

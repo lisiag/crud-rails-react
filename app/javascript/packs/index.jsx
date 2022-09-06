9a9d16cbf3fd26f4c115ba5b00249cd5fb499fe9
Lisia Grocott <lisiagrocott@gmail.com>
2022-09-06 20:53:02 +1200

    use createRoot

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "../components/index";
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(<App />, document.body.appendChild(document.createElement("div")));
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   render(<App />, document.body.appendChild(document.createElement("div")));
+// });
+
+const root = createRoot(document.documentElement);
+const element = <h1>Hello, world</h1>;
+root.render(element);
